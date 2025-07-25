bf1c61c8a3cd33b53fff9a409581237f250654d7
shashwatrajhack <er.shashwatraj@gmail.com>
2025-07-25 19:52:58 +0530

    building the search functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,19 @@
 import RestaurantCard from "./Restaurantcard";
-import { resList } from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => { 
 
-  const [listOfRestaurant,setListOfRestaurant] = useState(resList);
-  return (
+  const [listOfRestaurant,setListOfRestaurant] = useState([]);
+  useEffect(()=>{fetchData();},[])
+
+  const fetchData = async () => {
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+    const json = await data.json();
+    console.log(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+    setListOfRestaurant(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+  }
+
+  return listOfRestaurant.length === 0?<h1>Loading☠️☠️☠️☠️</h1>: (
     <div className="body">
       <div className="filter">
         <button className="filter-btn" onClick={()=>{const filteredRes = listOfRestaurant.filter((res) => res.info.avgRating > 4.3);
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
+
+  const [btnName,setBtnName] = useState("login")
   return (
     <div className="header">
       <div className="logo">
@@ -15,7 +18,13 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact us</li>
           <li>Cart</li>
+          <button className = "btn" onClick={()=>{
+            btnName === "Login" ?setBtnName("Logout"):setBtnName("Login");
+            console.log(btnName)
+          }}>{btnName}</button>
+          
         </ul>
+        
       </div>
     </div>
   );
