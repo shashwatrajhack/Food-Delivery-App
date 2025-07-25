165bc1ac5252638e386b8d1a3b348e1da91347b0
shashwatrajhack <er.shashwatraj@gmail.com>
2025-07-25 14:41:21 +0530

    filtering logic added

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,38 +1,16 @@
 import RestaurantCard from "./Restaurantcard";
 import { resList } from "../utils/mockData";
+import { useState } from "react";
 
-const Body = () => {
-  let listOfRestaurant = [
-    {
-      info: {
-        id: "156663",
-        name: "Sri Lakshmi Vaibhava",
-        cloudinaryImageId: "hxdoygnqwwi6i6fenhjo",
-        locality: "Marathahalli",
-        areaName: "Marathahalli",
-        costForTwo: "₹200 for two",
-        cuisines: ["South Indian"],
-        avgRating: 4.5,
-      },
-    },
-    {
-      info: {
-        id: "156643",
-        name: "Sri Narayana",
-        cloudinaryImageId: "hxdoygnqwwi6i6fenhjo",
-        locality: "Marathahalli",
-        areaName: "Marathahalli",
-        costForTwo: "₹200 for two",
-        cuisines: ["South Indian"],
-        avgRating: 3.5,
-      },
-    }
-  ]; 
+const Body = () => { 
+
+  const [listOfRestaurant,setListOfRestaurant] = useState(resList);
   return (
     <div className="body">
       <div className="filter">
-        <button className="filter-btn" onClick={()=>{listOfRestaurant = listOfRestaurant.filter((res) => res.info.avgRating > 4);
-            console.log(listOfRestaurant);
+        <button className="filter-btn" onClick={()=>{const filteredRes = listOfRestaurant.filter((res) => res.info.avgRating > 4.3);
+            console.log(filteredRes);
+            setListOfRestaurant(filteredRes)
         }}>Top Rated Restaurant</button>
       </div>
       <div className="res-container">
